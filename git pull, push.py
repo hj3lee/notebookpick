2a19e7d17099ddaf131e352888c1f10d7b8a42f4
hj3lee <hj3leee@gmail.com>
2026-03-31 14:20:58 +0900

    update

--- git pull, push.py
+++ git pull, push.py
@@ -8,14 +8,10 @@ import os
 
 os.chdir(r"C:\notebookpick")
 
-# 전체 변경 반영
 os.system("git add .")
 
-# 커밋 (변경 없으면 무시)
 os.system('git commit -m "update"')
 
-# 최신 반영 (rebase)
 os.system("git pull --rebase")
 
-# 푸시
 os.system("git push")
